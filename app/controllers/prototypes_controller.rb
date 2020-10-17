450307b0ee73109a4334f1e65228bc4e146b923d
KatoSayaka <on85.sayaka.kato@gmail.com>
2020-10-17 18:06:49 +0900

    新規投稿_修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -14,7 +14,7 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path
     else
-      render :new_prototype_path
+      render :new
     end
   end
 
